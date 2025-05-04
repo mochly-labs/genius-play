ec378eb7a633f086e0f7564788a18828fa5b1167
Hiroshi <yukicaffwy@gmail.com>
2025-05-04 12:24:26 -0300

    Update Builder

--- build-for-all.sh
+++ build-for-all.sh
@@ -7,17 +7,18 @@ print_message() {
     timestamp=$(date +"%Y-%m-%d %H:%M:%S")
     echo -e "${color}[${timestamp}] ${message}\033[0m"
 }
+
 build_for_os() {
     local os=$1
     local arch=$2
     local output=$3
 
     print_message "Building for $os ($arch)..." "\033[0;33m"
-    
+
     if [ "$os" = "windows" ]; then
-        GOOS=$os GOARCH=$arch GO111MODULE=on CGO_ENABLED=1 go build -o "dist/$output" -ldflags="-H=windowsgui"
+        GOOS=$os GOARCH=$arch CGO_ENABLED=0 go build -o "dist/$output" -ldflags="-H=windowsgui"
     else
-        GOOS=$os GOARCH=$arch GO111MODULE=on CGO_ENABLED=1 go build -o "dist/$output"
+        GOOS=$os GOARCH=$arch CGO_ENABLED=0 go build -o "dist/$output"
     fi
 
     if [ $? -eq 0 ]; then
@@ -27,12 +28,20 @@ build_for_os() {
     fi
 }
 
+# If "maconly" is passed, only build mac targets
+if [ "$1" = "maconly" ]; then
+    build_for_os "darwin" "amd64" "Genius Play! (64-bits macOS)"
+    build_for_os "darwin" "arm64" "Genius Play! (ARM64 macOS)"
+    exit 0
+fi
 
-build_for_os "windows" "386" "Passa ou Repassa (32-bits).exe"
-build_for_os "windows" "amd64" "Passa ou Repassa (64-bits).exe"
-build_for_os "linux" "amd64" "Passa ou Repassa (64-bits Linux)"
-build_for_os "linux" "386" "Passa ou Repassa (32-bits Linux)"
-build_for_os "darwin" "amd64" "Passa ou Repassa (64-bits macOS)"
-
+# Windows
+build_for_os "windows" "386" "Genius Play! (32-bits).exe"
+build_for_os "windows" "amd64" "Genius Play! (64-bits).exe"
+# Linux
+build_for_os "linux" "amd64" "Genius Play! (64-bits Linux)"
+build_for_os "linux" "386" "Genius Play! (32-bits Linux)"
+build_for_os "linux" "arm64" "Genius Play! (ARM64 Linux)"
+# macOS done in separate job
 
 print_message "Build process completed." "\033[0;36m"
